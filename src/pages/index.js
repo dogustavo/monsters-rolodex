4a17ab7cdc4ca901e7c30b53ed307ce47b958cd6
gustavo <gustavo.oliveira@atuo.in>
2020-04-24 21:06:33 -0300

    iniciado o projeto de acordo com a video aula

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
-function Home() {
-    return (
-        <h1>Olá</h1>
-    );
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            monsters: [
+                {
+                    name: 'Frankenstein',
+                    id: '21'
+                },
+                {
+                    name: 'Dracula',
+                    id: '22'
+                },
+                {
+                    name: 'Zombie',
+                    id: '23'
+                },
+
+            ]
+        };
+    }
+    render() {
+        return (
+            <section className="Home">
+                {
+                   this.state.monsters.map(
+                   monster => <h1 key={monster.id}> { monster.name } </h1>
+                    )
+                }
+            </section>
+        );
+    }
 }
 
 export default Home;
